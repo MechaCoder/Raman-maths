ae2e303c1e6396a3aa613a23acd814bd876f1526
MechaCoder <talktome1224@gmail.com>
2014-07-11 08:47:39 +0100

    more Devopment on the JS and the Qwestion syntaxt now works i will begin  to build the app.

---
+++ JScript.js
@@ -0,0 +1,11 @@
+var Q_Val_1 = 0;
+var Q_Val_2 = 0;
+var aws = 0;
+
+function NewQ(){
+	Q_Val_1 = Math.floor(Math.random()*6);
+	Q_Val_2 = Math.floor(Math.random()*6);
+	aws = Q_Val_1 * Q_Val_2;}
+	
+
+
